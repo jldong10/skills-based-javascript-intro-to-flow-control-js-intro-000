b90f53a05045bc88bb796d18cbc37ab2cb020524
steadfast-method-9892 <dong.jason86@gmail.com>
2020-08-27 03:43:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -34,9 +34,9 @@ function switchAge(age) {
     case 17:
       {return "You are a teenager"};
       break;
-    case 18:
-      {return "You are a teenager"};
-      break;
+              case 18:
+                {return "You are a teenager"};
+                break;
     case 19:
       {return "You are a teenager"};
       break;
